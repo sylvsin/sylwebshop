6bfb5296bdf0b332047f6f530fd0364f849c4b61
sylvain lubwimi <lubwimi@gmail.com>
2020-12-24 23:10:55 +0100

    add redux to filter component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 /*jshint esversion: 8 */
 import React from 'react';
 import './App.css';
-import data from './data.json';
+// import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/Cart';
@@ -14,9 +14,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem("cartItems")?JSON.parse(localStorage.getItem("cartItems")):[],
-      size: '',
       sort: '',
     };
   }
@@ -47,46 +45,6 @@ class App extends React.Component {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
-  sortProducts = (event) => {
-    //impl
-    console.log(event.target.value);
-
-    const sort = event.target.value;
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-      .slice()
-      .sort((a, b) => 
-        sort === "lowest" 
-        ? a.price > b.price
-          ? 1
-          : -1
-        : sort === "highest" 
-        ? a.price < b.price
-          ? 1
-          : -1
-        : a._id < b._id
-          ? 1
-          : -1
-      )
-    }))
-  }
-
-  filterProducts = (event) => {
-    //impl
-    console.log( event.target.value );
-
-    if(event.target.value === "") {
-      this.setState({size: event.target.value, products:data.products});
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0),
-      });
-    }
-    
-  };
-
   render() {
     return (
       <Provider store={store}>
@@ -98,16 +56,10 @@ class App extends React.Component {
             <div className="content">
               <div className="main">
 
-                <Filter count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                ></Filter>
-                <Products products={this.state.products} 
-                    addToCart={this.addToCart}
-                  ></Products> 
+                <Filter/>
+                <Products addToCart={this.addToCart}/> 
               </div>
+              
 
               <div className="sidebar">
                 <Cart 
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,16 +1,23 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
-import { FETCH_PRODUCTS }  from '../types';
+import { FETCH_PRODUCTS, FILTER_PRODUCT }  from '../types';
 
 export const fetchProducts = () => async(dispatch) => {
     const res = await fetch("/api/products");
     const data = await res.json();
-    // console.log(data);
     dispatch({
         type:FETCH_PRODUCTS,
         payload: data,
     });
-    console.log(data);
 };
 
+export const onFilterProduct = (filter)=> async(dispatch)=>{
+    const res = await fetch("/api/products");
+    const data = await res.json();
 
+    dispatch({
+        type:FILTER_PRODUCT,
+        payload: data.products.filter((product) => filter === "ALL"? true
+        : product.availableSizes.indexOf(filter)>=0)
+    })  
+}
