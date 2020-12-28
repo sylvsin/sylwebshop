1d804cee6ca1248c6364d471a7c5cfc048b82048
sylvain lubwimi <lubwimi@gmail.com>
2020-12-28 18:45:35 +0100

    create order

--- src/types.js
+++ src/types.js
@@ -4,3 +4,6 @@ export const FILTER_PRODUCT = "FILTER_PRODUCT";
 export const SORT_PRODUCT = "SORT_PRODUCT";
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CREATE_ORDER = "CREATE_ORDER";
+export const CLEAR_ORDER = "CLEAR_ORDER";
+export const CLEAR_CART = "CLEAR_CART";
